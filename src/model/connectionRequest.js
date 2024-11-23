7d3f450832d56f7889c39d02f4d8bc053845147a
nikhil <nikhilchawla9013@gmail.com>
2024-11-23 16:53:27 +0530

    add connection request schema

---
+++ src/model/connectionRequest.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const connectionRequestSchema = new mongoose.Schema({
+    fromUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true
+    },
+    toUserId: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true
+    },
+    status: {
+        type: String,
+        require: true,
+        enum: {
+            values:["ignored","interested","accepted","rejected"],
+            message: `{VALUE} is incorrect status type`
+        }
+    }
+},
+{
+    timestamps: true
+});
+
+const connectionRequestModel = new mongoose.model(
+    "ConnectionRequest", connectionRequestSchema
+);
+
+module.exports = connectionRequestModel
