bc178a497dd567a8c42b99bded0f10609ec9d5eb
nikhil <nikhilchawla9013@gmail.com>
2024-12-06 17:39:27 +0530

    fixed validation file

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,50 +1,51 @@
 const validator = require("validator");
 const validateSignUpData = (req) => {
-    const {firstName , lastName, emailId, password} = req.body;
-    if(!firstName || !lastName) {
-        throw new Error("Name is not valid");
-    }
+  const { firstName, lastName, emailId, password } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid");
+  }
 
-    if(!validator.isEmail(emailId)) {
-        throw new Error("Email is not valid");
-    }
+  if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid");
+  }
 
-    if(!validator.isStrongPassword(password)) {
-        throw new Error("Please enter a strong password");
-    }
-}
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong password");
+  }
+};
 
 const validateLoginData = (req) => {
-    const {emailId, password} = req.body;
-    if(!validator.isEmail(emailId)) {
-        throw new Error("Email is not valid");
-    }
+  const { emailId, password } = req.body;
+  if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid");
+  }
 
-    if(!validator.isStrongPassword(password)) {
-        throw new Error("Please enter a strong password");
-    }
-}
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong password");
+  }
+};
 
 const validateEditProfileData = (req) => {
-    const allowedEditableFields = [
-        "firstName",
-        "lastName",
-        "emailId",
-        "age",
-        "gender",
-        "photoUrl",
-        "skills"
-    ];
+  const allowedEditableFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "age",
+    "gender",
+    "photoUrl",
+    "skills",
+  ];
 
-
-    const isEditAllowed = Object.keys(req.body)?.every((key)=>allowedEditableFields?.includes(key));
-    if(!isEditAllowed) {
-        throw new Error("Invalid edit request");
-    }
-}
+  const isEditAllowed = Object.keys(req.body)?.every((key) =>
+    allowedEditableFields?.includes(key)
+  );
+  if (!isEditAllowed) {
+    throw new Error("Invalid edit request");
+  }
+};
 
 module.exports = {
-    validateSignUpData,
-    validateLoginData,
-    validateEditProfileData
-}
+  validateSignUpData,
+  validateLoginData,
+  validateEditProfileData,
+};
