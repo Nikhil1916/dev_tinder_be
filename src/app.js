24eca4f7e81268276dcad3286d430cff317a197c
nikhil <nikhilchawla9013@gmail.com>
2025-01-13 22:19:49 +0530

    Revert "add env variables"
    This reverts commit 194f2b17c4f39316de483ae80403a3ed12a176da.

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// require("dotenv").config();
 const { connectDB } = require("./config/database");
 const cookieParser = require("cookie-parser");
 const { authRouter } = require("./routes/auth");
 const { profileRouter } = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+require("dotenv").config();
 const corsOptions = {
   origin: 'http://localhost:5173', // Replace with your frontend's URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed HTTP methods
